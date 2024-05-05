f49c4673aae372d590bed7f8157f9a2a3c34b50d
Indra Mpd <indrampd21@gmail.com>
2024-05-05 09:44:37 +0700

    preparation for vercel

---
+++ app/ui/fonts.ts
@@ -0,0 +1,15 @@
+import {Inter, Lusitana} from 'next/font/google';
+
+export const inter = Inter({
+  subsets: [
+    'latin',
+    'latin-ext',
+  ],  
+})
+
+export const lusitana = Lusitana({
+  subsets: [
+    'latin',
+  ],
+  weight: ['400', '700'],
+})
