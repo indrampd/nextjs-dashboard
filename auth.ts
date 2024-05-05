7a98a3ac77281fe773937d2082813ffa3415b01f
indrampd <indrampd21@gmail.com>
2024-05-05 16:30:22 +0700

    chore: Update password validation to require a minimum of 6 characters

--- auth.ts
+++ auth.ts
@@ -22,7 +22,7 @@ export const { auth, signIn, signOut } = NextAuth({
     async authorize(credentials) {
       const parsedCrendentials = z.object({
         email: z.string().email(),
-        password: z.string(),
+        password: z.string().min(6),
       }).safeParse(credentials);
 
       if (parsedCrendentials.success) {
